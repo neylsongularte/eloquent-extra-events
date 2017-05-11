47340fb2b5829dc972bd32ec3127b900aaddfa28
Neylson O. Gularte <neylsond@gmail.com>
2017-05-11 14:13:55 -0400

    melhoria

--- src/ExtraEventsTrait.php
+++ src/ExtraEventsTrait.php
@@ -10,47 +10,18 @@ trait ExtraEventsTrait {
 
 
 
-
     public function belongsToMany($related, $table = null, $foreignKey = null, $otherKey = null, $relation = null)
     {
 
-        // Laravel 5.2 and 5.3 code
-        if(str_contains(App::VERSION(), ['5.2.', '5.3.'])) {
-            if (is_null($relation)) {
-                $relation = $this->getBelongsToManyCaller();
-            }
+        $belongsToMany = parent::belongsToMany($related, $table, $foreignKey, $otherKey, $relation);
 
-            $foreignKey = $foreignKey ?: $this->getForeignKey();
-            $instance = new $related;
-            $otherKey = $otherKey ?: $instance->getForeignKey();
+        $query = $belongsToMany->getQuery()->getModel()->newQuery();
+        $parent = $belongsToMany->getParent();
+        $table = $belongsToMany->getTable();
+        $foreignKey = explode('.', $belongsToMany->getForeignKey())[1];
+        $otherKey = explode('.', $belongsToMany->getOtherKey())[1];
+        $relation = $belongsToMany->getRelationName();
 
-            if (is_null($table)) {
-                $table = $this->joiningTable($related);
-            }
-
-            $query = $instance->newQuery();
-
-            return new BelongsToMany($query, $this, $table, $foreignKey, $otherKey, $relation);
-        } else if(str_contains(App::VERSION(), '5.4.')) {
-
-            if (is_null($relation)) {
-                $relation = $this->guessBelongsToManyRelation();
-            }
-
-            $instance = $this->newRelatedInstance($related);
-            $foreignKey = $foreignKey ?: $this->getForeignKey();
-            $relatedKey = $relatedKey ?: $instance->getForeignKey();
-
-            if (is_null($table)) {
-                $table = $this->joiningTable($related);
-            }
-
-            return new BelongsToMany(
-                $instance->newQuery(), $this, $table, $foreignKey, $relatedKey, $relation
-            );
-        }
-
-
-        throw new Exception('Versão não suportada');
+        return new BelongsToMany($query, $parent, $table, $foreignKey, $otherKey, $relation);
     }
 }
